7d9e99c04e6450a8c6fabbb9857adf8c6388bdf3
Rafat Parween <109865229+rafatparween@users.noreply.github.com>
2025-08-09 13:17:32 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -7,8 +7,8 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "hewlett-printer-solution.online",
-  description: "hewlett-printer-solution.online",
+  title: "	123printerssetupsteps",
+  description: "123printerssetupsteps",
 };
 
 export default function RootLayout({ children }) {
